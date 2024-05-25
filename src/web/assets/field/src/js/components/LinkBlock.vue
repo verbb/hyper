e1a01f31168edc652b37b9513b5e56b8aef81321
Josh Crawford <joshua@sgroup.com.au>
2024-05-25 22:50:32 +1000

    Fix link type slide-out button not working correctly

--- src/web/assets/field/src/js/components/LinkBlock.vue
+++ src/web/assets/field/src/js/components/LinkBlock.vue
@@ -16,7 +16,7 @@
                     </lightswitch-field>
                 </div>
 
-                <template v-if="!settings.isStatic || settings.multipleLinks">
+                <template v-if="!settings.isStatic && (!settings.multipleLinks && linkType.tabCount > 1) || settings.multipleLinks">
                     <div v-if="settings.multipleLinks && linkType.tabCount > 1" class="hyper-header-settings ellipsis" @click.prevent="openSettings">
                         <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M8 256a56 56 0 1 1 112 0A56 56 0 1 1 8 256zm160 0a56 56 0 1 1 112 0 56 56 0 1 1 -112 0zm216-56a56 56 0 1 1 0 112 56 56 0 1 1 0-112z" /></svg>
                     </div>
