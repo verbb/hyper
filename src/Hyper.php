9fb37c7cd0a02ec197d9c5b6fbd990493f0187c8
Josh Crawford <joshua@sgroup.com.au>
2026-09-09 20:08:05 +1000

    Normalize plugin settings.

--- src/Hyper.php
+++ src/Hyper.php
@@ -111,6 +111,9 @@ class Hyper extends Plugin
         Event::on(UrlManager::class, UrlManager::EVENT_REGISTER_CP_URL_RULES, function(RegisterUrlRulesEvent $event) {
             $event->rules['hyper'] = 'hyper/plugin/settings';
             $event->rules['hyper/settings'] = 'hyper/plugin/settings';
+            $event->rules['hyper/settings/migrate/linkit'] = ['template' => 'hyper/settings/migrate/linkit'];
+            $event->rules['hyper/settings/migrate/typed-link'] = ['template' => 'hyper/settings/migrate/typed-link'];
+            $event->rules['hyper/settings/migrate/link'] = ['template' => 'hyper/settings/migrate/link'];
         });
     }
 
